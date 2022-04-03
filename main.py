ce204df67b88dc0f9147105b4db213d2ca056d73
DestaDag <dagimay1@gmail.com>
2022-04-03 23:49:53 +0200

    implemented encryption func

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from operator import xor
 import random
 import string
 
@@ -5,8 +6,25 @@ def random_key(text_len):
     return ''.join(random.SystemRandom().choice(string.ascii_lowercase) for _ in range(text_len))
 
 
+def change_to_hex(strng):
+    return hex(int(strng, base=16))
+
+def encrypt(text, key_text):
+    enc = [(ord(a) ^ ord(b)) for a, b in zip(text, key_text)]
+    for i in range(len(enc)):
+        if enc[i] > 26:
+            enc[i] = enc[i] - 26
+        enc[i] = chr((enc[i]+ 96))
+    return ''.join(enc)
+
+
 def main():
-    print(random_key(len('dagim')))
+    message = ""
+    key = random_key(len(message))
+    print("message: ",message)
+    print("key: ", key)
+    print("pad: ", encrypt(message, key))
+
 
 
 if __name__ == "__main__":
