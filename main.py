acf7f03d0d306a517e4ab488f1a84998c7eac3c4
DestaDag <dagimay1@gmail.com>
2022-04-04 11:24:21 +0200

    added decrypt func

--- main.py
+++ main.py
@@ -11,19 +11,34 @@ def change_to_hex(strng):
 
 def encrypt(text, key_text):
     enc = [(ord(a) ^ ord(b)) for a, b in zip(text, key_text)]
+    print(enc)
     for i in range(len(enc)):
-        if enc[i] > 26:
-            enc[i] = enc[i] - 26
+        #if enc[i] > 26:
+            #enc[i] = enc[i] %26
         enc[i] = chr((enc[i]+ 96))
     return ''.join(enc)
+
+def decrypt(pad, key_text):
+    enc = [(ord(a) ^ ord(b)) for a, b in zip(pad, key_text)]
+    print(enc)
+    for i in range(len(enc)):
+        if enc[i] > 26:
+            while(enc[i]> 26):
+                enc[i] = enc[i] %26
+        enc[i] = chr((enc[i]+ 96))
+    return ''.join(enc)
+
 
 
 def main():
     message = ""
     key = random_key(len(message))
+    pad = encrypt(message, key)
+    decry = decrypt(pad, key)
     print("message: ",message)
     print("key: ", key)
-    print("pad: ", encrypt(message, key))
+    print("pad: ", pad)
+    print("decrypt: ", decry)
 
 
 
